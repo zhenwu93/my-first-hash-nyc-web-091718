f1779a297d6cebf2924a6ccd006198dcf4a28c25
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-13 01:46:30 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -21,7 +21,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-the_manifest["oil paintings"]
+shipping_manifest["oil paintings"]
 end
 
 def adding
