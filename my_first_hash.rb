5d4ca8b4b336b96a6ec9895e759bf0ff2dd88285
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-13 01:35:51 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,7 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+name = {"first_name" => "Mike", "last_name" => "Wu" }
 
-  
 end
 
 
